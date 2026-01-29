320ec774d5caced121194a287b397054bd4ce093
UddhavJoshi <90190402+Mr-HECKER147@users.noreply.github.com>
2026-01-29 18:42:58 +0530

    Added address based size change and 1 based index input

--- Array/ArrayFuncs.c
+++ Array/ArrayFuncs.c
@@ -10,59 +10,60 @@ void deleteElement(int pos);
 void display();
 
 int arr[MAX_SIZE];
-int n;
 
-void write(int n) {
-    if (n > MAX_SIZE) {
+void write(int* n) {
+    if (*n > MAX_SIZE) {
         printf("Array size exceeds maximum size of %d\n", MAX_SIZE);
         return;
     }
 
     printf("Enter elements: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < *n; i++) {
         scanf("%d", &arr[i]);
     }
 }
 
-void insert(int pos, int val) {
-    if (pos < 0 || pos > n) {
+void insert(int pos, int val,int* n) {
+    pos--; // Convert to 0-based index
+    if (pos < 0 || pos > *n) {
         printf("Invalid position\n");
         return;
     }
 
-    for (int i = n; i > pos; i--) {
+    for (int i = *n; i > pos; i--) {
         arr[i] = arr[i - 1];
     }
     arr[pos] = val;
-    n++;
+    (*n)++;
 }
 
-void deleteElement(int pos) {
-    if (pos < 0 || pos >= n) {
+void deleteElement(int pos,int* n) {
+    pos--; // Convert to 0-based index
+    if (pos < 0 || pos >= *n) {
         printf("Invalid position\n");
         return;
     }
 
-    for (int i = pos; i < n - 1; i++) {
+    for (int i = pos; i < *n - 1; i++) {
         arr[i] = arr[i + 1];
     }
-    n--;
+    (*n)--;
 }
 
-void display() {
-    printf("Array elements (size of array: %d): [ ", n);
-    for (int i = 0; i < n; i++) {
+void display(int *n) {
+    printf("Array elements (size of array: %d): [ ", *n);
+    for (int i = 0; i < *n; i++) {
         printf("%d ", arr[i]);
     }
     printf("]\n");
 }
 int main(){
-    printf("Enter size of array: ");
-    
+    int n =0;
+    printf("Enter size of your array: ");
     scanf(" %d",&n);
-    write(n);
+    write(&n);
     while(1){
-        printf("\n\n1.Insert\n2.Delete\n3.Display\n4.Exit\n");
+        printf("\n1.Insert\n2.Delete\n3.Display\n4.Exit\n");
         int choice, val, pos;
         printf("Enter your choice: ");
         scanf(" %d",&choice);
@@ -72,15 +73,15 @@ int main(){
                 scanf(" %d",&val);
                 printf("Enter position to insert: ");
                 scanf(" %d",&pos);
-                insert(pos, val);
+                insert(pos, val,&n);
                 break;
             case 2:
                 printf("Enter position to delete: ");
                 scanf(" %d",&pos);
-                deleteElement(pos);
+                deleteElement(pos,&n);
                 break;
             case 3:
-                display();
+                display(&n);
                 break;
             case 4:
                 exit(0);
